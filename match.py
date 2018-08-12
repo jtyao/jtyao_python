5267a4817db36ffb66114c3701092d18075669e5
Juntao Yao <yaojuntao@huami.com>
2018-08-12 10:34:22 +0800

    初始化功能

---
+++ match.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from urllib.parse import quote
+import sys
+from pyquery import PyQuery as pq
+import re
+from selenium.webdriver.support.select import Select
+import time
+import random
+from lxml import etree
+
+def get_match_info(year, stage, text):
+    match = {
+        'year': year,
+        'stage': stage,
+    }
+
+    match['league'] = text[0]
+    match['date'] = text[1].replace("\n", " ", 1)
+    match['home_name'] = text[2].strip('21')
+    match['away_name'] = text[4].strip('21')
+    match['score'] = text[3]
+    match['half_score'] = text[10]
+
+    return match
+
+def get_odds(url):
+    odds_browser = webdriver.Firefox()
+    odds_wait = WebDriverWait(odds_browser, 10)
+
+    try:
+        odds_browser.get(url)
+        odds_wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#sel_showType')))
+
+        select = Select(odds_browser.find_element_by_id("sel_showType"))
+        select.select_by_value('1')
+        odds_wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#dataList')))
+
+        html = etree.HTML(odds_browser.page_source)
+        doc = pq(html)
+        td = doc('#oddstr_281 td')
+
+        text = []
+        odds = {}
+        for item in td.items():
+            text.append(item.text())
+        odds['start'] = text[2:5]
+
+        text = []
+        end_text = []
+        next = td.parent().next()
+        for item in next.items():
+            end_text.append(item.text())
+        text = end_text[0].split()
+        odds['end'] = text[0:3]
+
+    except TimeoutException:
+        get_odds(url)
+    time.sleep(random.randint(3, 8)) 
+    odds_browser.close()
+    return odds
+
+def get_over_down(url):
+    odds_browser = webdriver.Firefox()
+    odds_wait = WebDriverWait(odds_browser, 10)
+
+    try:
+        odds_browser.get(url)
+        odds_wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#odds')))
+
+        html = etree.HTML(odds_browser.page_source)
+        doc = pq(html)
+        tr = doc('#odds tbody tr')
+
+        text = []
+        odds = {}
+        for item in tr.items():
+            for td in item.find('td').items():
+                result = re.search('Bet365', td.text())
+                if result:
+                    bet365 = td.parent()
+                    for bet365_td in bet365.find('td').items():
+                        text.append(bet365_td.text())
+                    break
+
+        if len(text) < 10:
+            print("大小球赔率页面错误")
+            sys.exit()
+        odds['start'] = text[2:5]
+        odds['end'] = text[8:11]
+
+    except TimeoutException:
+        get_over_down(url)
+    time.sleep(random.randint(3, 8)) 
+    odds_browser.close() 
+    return odds
+
+def get_match(page, year, stage):
+    html = etree.HTML(page.page_source)
+    doc = pq(html)
+    has_odds = False
+    has_OD = False
+
+    items = doc('#Table3 tr a').parent().parent()
+
+
+    for item in items.items():
+        text = []
+        tds = item.find('td')
+        a = tds.find('a')
+        for td in tds.items():
+            text.append(td.text())
+
+        if len(text) == 11:
+            match = get_match_info(year, stage, text)
+
+            for item in a.items():
+                url = item.attr('href')
+
+                result = re.search('oddslist', url)
+                if result:
+                    has_odds = True
+                    odds = []
+                    odds = get_odds(url)
+                    match['odds'] = odds
+
+                result = re.search('OverDown', url)
+                if result:
+                    has_OD = True
+                    over_down = get_over_down(url)
+                    match['over_down'] = over_down
+
+            print(match)
+        elif len(text) != 0:
+            print("比赛数据错误")
+            sys.exit()
---
+++ world_cup.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from urllib.parse import quote
+import sys
+from pyquery import PyQuery as pq
+import re
+from selenium.webdriver.support.select import Select
+import match
+import time
+import random
+
+match_year = ['2018']
+#profile = webdriver.FirefoxProfile()
+#profile.set_preference('network.proxy.type', 1)
+#profile.set_preference('network.proxy.http', '117.127.0.205')
+#profile.set_preference('network.proxy.http_port', 80)
+#profile.set_preference('network.proxy.ssl', '117.127.0.205')
+#profile.set_preference('network.proxy.ssl_port', 80)
+#profile.update_preferences()
+#browser = webdriver.Firefox(profile)
+browser = webdriver.Chrome()
+wait = WebDriverWait(browser, 10)
+
+def get_stage1(year, stage):
+    print("stage1")
+    for group in range(0, 8):
+        clicks = browser.find_elements(By.CSS_SELECTOR, '#showRound tr .lsm2')
+        if (len(clicks) != 9):
+            print("小组页面按钮个数不对")
+            sys.exit()
+
+        clicks[group].click()
+        check = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainTableDiv')))
+        print(group)
+        time.sleep(random.randint(3, 8))
+        match.get_match(browser, year, stage)
+
+def get_stage(year):
+    for stage in range(0, 6):
+        clicks = browser.find_elements(By.CSS_SELECTOR, '#SubSelectDiv td[class^="cupmatch_rw2"]')
+
+        if (len(clicks) != 6):
+            print("页面按钮个数不对" + str(len(clicks)))
+            sys.exit()
+
+        clicks[stage].click()
+        check = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainTableDiv')))
+        time.sleep(random.randint(3, 8)) 
+
+        if stage == 0:
+            get_stage1(year, "小组赛")
+        elif stage == 1:
+            match.get_match(browser, year, "16强")
+        elif stage == 2:
+            match.get_match(browser, year, "8强")
+        elif stage == 3:
+            match.get_match(browser, year, "4强")
+        elif stage == 4:
+            match.get_match(browser, year, "季军赛")
+        elif stage == 5:
+            match.get_match(browser, year, "决赛")
+
+
+def index_year(year):
+    print("正在爬取第", year, '年')
+    try:
+        url = 'http://zq.win007.com/cn/CupMatch/%s/75.html' %(year)
+        browser.get(url)
+        check = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#SubSelectDiv')))
+        get_stage(year)
+    except TimeoutException:
+        index_year(year)
+
+def main():
+    for i in match_year:
+        index_year(i)
+
+if __name__ == '__main__':
+    main()
